c84b0516f04a512e1799956c214d4300febf5ffb
jdrodriguez10 <jdrodriguez10@miners.utep.edu>
2020-11-29 01:06:42 -0700

    Created new directory called project and transfered files from project 2. Added new files and created wdt_c_handler function to toyManin.c. Created a Makefile and added wdt_handler.s file to project directory.

---
+++ project/toyMain.c
@@ -0,0 +1,33 @@
+#include <msp430.h>
+#include "libTimer.h"
+#include "led.h"
+#include "buzzer.h"
+#include "switches.h"
+#include "stateMachines.h"
+#include "lcdutils.h"
+#include "lcddraw.h"
+
+short redrawScreen = 1;
+
+void wdt_c_handler()
+{
+  static int secCount = 0;
+  secCount ++;
+  if (secCount == 1) {
+    secCount = 0;
+    state_advance();
+    redrawScreen = 1;
+  }
+}
+
+void main()
+{
+  configureClocks();           /* setup master oscillator, CPU & peripheral clocks */
+  led_init();
+  buzzer_init();
+  switch_init();
+  lcd_init();
+  enableWDTInterrupts();       /* enable periodic interrupt */
+  or_sr(0x08);                 /* GIE*/
+  clearScreen(COLOR_BLACK);
+}

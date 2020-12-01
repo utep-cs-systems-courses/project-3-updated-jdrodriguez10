f7ace9c02f221d242cc3e1af1f70f8fb9b1554a8
jdrodriguez10 <jdrodriguez10@miners.utep.edu>
2020-11-30 23:32:45 -0700

    Added buzzer_advance.s to Makefile and added comments to buzzer_advance.s.

--- project/buzzer_advance.s
+++ project/buzzer_advance.s
@@ -9,19 +9,19 @@
 	.text
 	.global buzzer_advance
 	
-buzzer_advance:
-	cmp #0, &state2_status
+buzzer_advance:	
+	cmp #0, &state2_status	;lower freq if state2_status 0, not 0 raise freq
 	jz else
 
 if:
-	add #225, &freq
+	add #225, &freq		;raise freq
 	jmp exit
 else:
-	sub #450, &freq
+	sub #450, &freq		;lower freq
 
 exit:
 	mov &freq, r12
-	call #get_period
+	call #get_period	;call get_period 2000000/r12
 
-	call #buzzer_set_period
-	pop r0
+	call #buzzer_set_period	;call buzzer_set_period with result
+	pop r0			;return

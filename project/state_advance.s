dc536883bc1eb1fc71361400cb19dd3f212eb28a
jdrodriguez10 <jdrodriguez10@miners.utep.edu>
2020-11-30 20:40:09 -0700

    added comments to state_advance.s.

--- project/state_advance.s
+++ project/state_advance.s
@@ -15,27 +15,27 @@ jt:
 	.global state_advance
 state_advance:
 	sub #2, r1
-	mov #0, 0(r1)
+	mov #0, 0(r1)		  ;changed = 0
 	cmp.b #5, &super_state
 	jhs exit
 	mov.b &super_state, r12
 	add.b r12, r12
 	mov jt(r12), r0
 
-case1:	  call #state1
-	mov.b r12, 0(r1)
+case1:	call #state1
+	mov.b r12, 0(r1)	  ;changed = state1
 	jmp exit
-case2:	  call #state2
-	mov.b r12, 0(r1)
+case2:	call #state2
+	mov.b r12, 0(r1)	  ;changed = state2
 	jmp exit
-case3:	  call #state3
-	mov.b r12, 0(r1)
+case3:	call #state3
+	mov.b r12, 0(r1)	  ;changed = state3
 	jmp exit
-case4:	  call #state4
-	mov.b r12, 0(r1)
+case4:	call #state4
+	mov.b r12, 0(r1)	  ;changed = state4
 	jmp exit
 exit:
-	mov.b 0(r1), &led_changed
-	add #2, r1
+	mov.b 0(r1), &led_changed ;led_change = changed
+	add #2, r1		  ;moves stack pointer
 	call #led_update
-	pop r0
+	pop r0			  ;return
